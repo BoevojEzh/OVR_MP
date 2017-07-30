d4c9fd74981bcfd674a225a40bde8aff482646d1
BoevojEzh <zakhar.tremi@gmail.com>
2017-07-31 00:11:35 +0300

    Raw_Done

--- OVR_MP_Server/Assets/Scripts/NetworkManager_custom.cs
+++ OVR_MP_Server/Assets/Scripts/NetworkManager_custom.cs
@@ -12,8 +12,12 @@ public class NetworkManager_custom : NetworkManager
 
     public void StartupServer()
     {
+        SetIPAddress();
         SetPort();
+        NetworkManager.singleton.maxConnections = 4;
         NetworkManager.singleton.StartServer();
+        
+
     }
 
 
@@ -28,4 +32,28 @@ public class NetworkManager_custom : NetworkManager
         int port = int.Parse(GameObject.Find("InputFieldPort").transform.Find("Text").GetComponent<Text>().text);
         NetworkManager.singleton.networkPort = port;
     }
+
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        string playersCount = (NetworkManager.singleton.numPlayers + 1).ToString();
+        GameObject.Find("TextPlayers").transform.GetComponent<Text>().text = playersCount;
+        GameObject.Find("ServerInfoPanel").transform.Find("Player"+ playersCount +"Info").gameObject.SetActive(true);
+        GameObject.Find("ServerInfoPanel").transform.Find("Player" + playersCount + "Info").gameObject.transform.Find("PlayerText").GetComponent<Text>().text = conn.address; //returns IPv6 address instead of IPv4
+        GameObject.Find("ServerInfoPanel").transform.Find("Player" + playersCount + "Info").gameObject.name = "Player" + conn.connectionId + "Info";
+        Debug.Log("Player" + conn.connectionId + "Info");
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        string playersCount = (NetworkManager.singleton.numPlayers + 1).ToString();
+        GameObject.Find("ServerInfoPanel").transform.Find("Player" + conn.connectionId + "Info").gameObject.SetActive(false);
+        GameObject.Find("TextPlayers").transform.GetComponent<Text>().text = (NetworkManager.singleton.numPlayers).ToString();
+
+        Destroy(conn.playerControllers[0].gameObject);
+        NetworkServer.Destroy(conn.playerControllers[0].gameObject);
+
+    }
+
+
 }
---
+++ OVR_MP_Server/Assets/Scripts/ServerInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ServerInfo : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject.Find("TextServerIP").transform.GetComponent<Text>().text = NetworkManager.singleton.networkAddress + ":" + NetworkManager.singleton.networkPort;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       // GameObject.Find("TextPlayers").transform.GetComponent<Text>().text = NetworkManager.singleton.numPlayers.ToString();
+
+    }
+
+
+}
